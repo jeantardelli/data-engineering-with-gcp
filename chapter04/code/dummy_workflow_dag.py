eaa1e629f189440169932411ac8afee613a0810d
Jean Carlo <jeantardelli@gmail.com>
2023-02-17 22:31:25 +0000

    Dummy DAG workflow

---
+++ chapter04/code/dummy_workflow_dag.py
@@ -0,0 +1,31 @@
+"""This module contains creates a DAG using Python and explores
+basic components of Airflow such as timings and task dependencies."""
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+
+# Declares the DAG owner
+args = {"owner": "jtardelli"}
+
+# DAG declaration. It contains the time to start the DAG, the
+# interval and the DAG id.
+with DAG(
+    dag_id="hello_worrd",
+    default_args=args,
+    schedule_interval="0 5 * * *",  # minute, hour, day, month, and week day
+    start_date=days_ago(1),
+) as dag:
+
+    # Define BashOperator to print words
+    # BashOperator is an AirFlow operator to run Linux commands
+    print_hello = BashOperator(task_id="print_hello", bash_command="echo Hello")
+
+    print_world = BashOperator(task_id="print_world", bash_command="echo World")
+
+    # Define the bitwise operator to indicate the task dependency
+    print_hello >> print_world
+
+if __name__ == "__main__":
+    dag.cli()
---
+++ chapter04/scripts/set-environment-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export IMAGE_VERSION=composer-1.16.6-airflow-1.10.15
+export NODE_COUNT=3
+export DISK_SIZE=20
+export LOCATION=us-central1
+export ENVIRONMENT_NAME=composer-dev
+export MACHINE_TYPE=n1-standard-1
+export PYTHON_VERSION=3
