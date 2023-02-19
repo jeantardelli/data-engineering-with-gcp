3945393009820decbad8e791b1be4aee263062a4
Jean Carlo <jeantardelli@gmail.com>
2023-02-19 21:31:06 +0000

    Fix typos

--- basic-python-examples/scripts/create-multiple-datasets.sh
+++ basic-python-examples/scripts/create-multiple-datasets.sh
@@ -2,5 +2,5 @@
 
 
 for i in raw_bikesharing dwh_bikesharing dm_bikesharing; do
-    python ./basic-phython-examples/scripts/create_dataset.py -l ${LOCATION} -n ${i}
+    python ./basic-python-examples/scripts/create_dataset.py -l ${LOCATION} -n ${i}
 done
