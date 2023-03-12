a03e782aada8bbf789ca8157420bc41494816c58
Jean Carlo <jeantardelli@gmail.com>
2023-03-12 12:07:51 +0000

    IaC - GCS setup/deletion

--- data-lake-with-dataproc/scripts/environment_variables_setup.sh
+++ data-lake-with-dataproc/scripts/environment_variables_setup.sh
@@ -11,3 +11,8 @@ export DATAPROC_CLUSTER_WORKER_BOOT_DISK_SIZE=30
 export DATAPROC_CLUSTER_IMAGE_VERSION=2.0-debian10
 
 export GCP_PROJECT_ID=$(gcloud config get-value project)
+
+export GCS_BUCKET_NAME=dev-dataproc-cluster-${GCP_PROJECT_ID}
+export GCS_BUCKET_LOCATION=us-central1
+export GCS_DATA_SOURCE_PATH=./source/
+export GCS_BUCKET_OBJECT_PATH=source/csv/simple_sample.csv
